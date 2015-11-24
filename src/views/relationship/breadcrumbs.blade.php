1d3c8a0e60e29c2bc34d89d64604b8ca646c59cc
Jose Carlos Rodríguez Palacín <cpalacin@syscover.com>
2015-11-24 16:59:07 +0100

    minero fixed

--- src/views/relationship/breadcrumbs.blade.php
+++ src/views/relationship/breadcrumbs.blade.php
@@ -1,8 +1,8 @@
-<!-- mifinan::relationship.breadcrumbs -->
+<!-- hotels::relationship.breadcrumbs -->
 <li>
     <a href="javascript:void(0)">{{ trans('hotels::pulsar.package_name') }}</a>
 </li>
 <li class="current">
-    <a href="{{ route($routeSuffix) }}">{{ trans_choice($objectTrans, 2) }}</a>
+    <a href="{{ route($routeSuffix, [session('baseLang')]) }}">{{ trans_choice($objectTrans, 2) }}</a>
 </li>
-<!-- /mifinan::relationship.breadcrumbs -->
+<!-- /hotels::relationship.breadcrumbs -->
