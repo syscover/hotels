0115e151da6ab5c1d8d789d653c2ad9fff563ec3
Jose Carlos Rodríguez Palacín <cpalacin@syscover.com>
2015-12-16 19:57:23 +0100

    set slug in list services

--- src/views/service/index.blade.php
+++ src/views/service/index.blade.php
@@ -10,9 +10,9 @@
                 $('.datatable-pulsar').dataTable({
                     'iDisplayStart' : {{ $offset }},
                     'aoColumnDefs': [
-                        { 'bSortable': false, 'aTargets': [4,5]},
-                        { 'sClass': 'checkbox-column', 'aTargets': [4]},
-                        { 'sClass': 'align-center', 'aTargets': [5]}
+                        { 'bSortable': false, 'aTargets': [5,6]},
+                        { 'sClass': 'checkbox-column', 'aTargets': [5]},
+                        { 'sClass': 'align-center', 'aTargets': [6]}
                     ],
                     "bProcessing": true,
                     "bServerSide": true,
@@ -27,10 +27,11 @@
 @section('tHead')
     <!-- hotels::services.index -->
     <tr>
-        <th data-hide="phone,tablet">ID.</th>
+        <th>ID.</th>
         <th data-hide="phone,tablet">{{ trans_choice('pulsar::pulsar.language', 1) }}</th>
         <th data-class="expand">{{ trans('pulsar::pulsar.name') }}</th>
-        <th data-class="expand">{{ trans_choice('pulsar::pulsar.icon', 1) }}</th>
+        <th data-hide="phone">{{ trans('pulsar::pulsar.slug') }}</th>
+        <th data-hide="phone">{{ trans_choice('pulsar::pulsar.icon', 1) }}</th>
         <th class="checkbox-column"><input type="checkbox" class="uniform"></th>
         <th>{{ trans_choice('pulsar::pulsar.action', 2) }}</th>
     </tr>
