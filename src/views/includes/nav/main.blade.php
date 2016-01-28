7f95ba94bd40be51567a2852b2887897fe419e7d
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-01-28 11:31:12 +0100

    upgrade to laravel 5.2

--- src/views/includes/nav/main.blade.php
+++ src/views/includes/nav/main.blade.php
@@ -1,25 +1,25 @@
         <li{!! Miscellaneous::setCurrentOpenPage(['hotels-environment','hotels-decoration','hotels-service', 'hotels-relationship', 'hotels-hotel','hotels-publication','hotels-attachment-family']) !!}>
             <a href="javascript:void(0)"><i class="fa fa-h-square"></i>{{ trans('hotels::pulsar.package_name') }}</a>
             <ul class="sub-menu">
-                @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'hotels-hotel', 'access'))
+                @if(session('userAcl')->allows('hotels-hotel', 'access'))
                     <li{!! Miscellaneous::setCurrentPage('hotels-hotel') !!}><a href="{{ route('Hotel', [session('baseLang')]) }}"><i class="fa fa-h-square"></i>{{ trans_choice('hotels::pulsar.hotel', 2) }}</a></li>
                 @endif
                 <li{!! Miscellaneous::setCurrentOpenPage(['hotels-environment','hotels-decoration','hotels-relationship','hotels-service','hotels-publication','hotels-attachment-family']) !!}>
                     <a href="javascript:void(0)"><i class="icomoon-icon-grid"></i>{{ trans('pulsar::pulsar.master_tables') }}</a>
                     <ul class="sub-menu" >
-                        @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'hotels-publication', 'access'))
+                        @if(session('userAcl')->allows('hotels-publication', 'access'))
                             <li{!! Miscellaneous::setCurrentPage('hotels-publication') !!}><a href="{{ route('HotelsPublication') }}"><i class="fa fa-object-ungroup"></i>{{ trans_choice('hotels::pulsar.publication', 2) }}</a></li>
                         @endif
-                        @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'hotels-service', 'access'))
+                        @if(session('userAcl')->allows('hotels-service', 'access'))
                             <li{!! Miscellaneous::setCurrentPage('hotels-service') !!}><a href="{{ route('HotelsService', [session('baseLang')]) }}"><i class="icomoon-icon-wand-2"></i>{{ trans_choice('hotels::pulsar.service', 2) }}</a></li>
                         @endif
-                        @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'hotels-decoration', 'access'))
+                        @if(session('userAcl')->allows('hotels-decoration', 'access'))
                             <li{!! Miscellaneous::setCurrentPage('hotels-decoration') !!}><a href="{{ route('HotelsDecoration', [session('baseLang')]) }}"><i class="fa fa-lightbulb-o"></i>{{ trans_choice('hotels::pulsar.decoration', 2) }}</a></li>
                         @endif
-                        @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'hotels-environment', 'access'))
+                        @if(session('userAcl')->allows('hotels-environment', 'access'))
                             <li{!! Miscellaneous::setCurrentPage('hotels-environment') !!}><a href="{{ route('HotelsEnvironment', [session('baseLang')]) }}"><i class="fa fa-picture-o"></i>{{ trans_choice('hotels::pulsar.environment', 2) }}</a></li>
                         @endif
-                        @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'hotels-relationship', 'access'))
+                        @if(session('userAcl')->allows('hotels-relationship', 'access'))
                             <li{!! Miscellaneous::setCurrentPage('hotels-relationship') !!}><a href="{{ route('HotelsRelationship', [session('baseLang')]) }}"><i class="fa fa-group"></i>{{ trans_choice('hotels::pulsar.relationship', 2) }}</a></li>
                         @endif
                     </ul>
