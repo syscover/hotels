d1406562cc2666dac883a63be3da3e3c1861b376
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-03-05 11:51:33 +0100

    update service provider

--- src/Syscover/Hotels/HotelsServiceProvider.php
+++ src/Syscover/Hotels/HotelsServiceProvider.php
@@ -12,33 +12,34 @@ class HotelsServiceProvider extends ServiceProvider
 	public function boot()
 	{
 		// include route.php file
-		include realpath(__DIR__ . '/../../routes.php');
+		if (!$this->app->routesAreCached())
+			require __DIR__ . '/../../routes.php';
 
 		// register views
-		$this->loadViewsFrom(realpath(__DIR__ . '/../../views'), 'hotels');
+		$this->loadViewsFrom(__DIR__ . '/../../views', 'hotels');
 
         // register translations
-        $this->loadTranslationsFrom(realpath(__DIR__ . '/../../lang'), 'hotels');
+        $this->loadTranslationsFrom(__DIR__ . '/../../lang', 'hotels');
 
         // register public files
         $this->publishes([
-            realpath(__DIR__ . '/../../../public') => public_path('/packages/syscover/hotels')
+            __DIR__ . '/../../../public' 					=> public_path('/packages/syscover/hotels')
         ]);
 
         // register config files
         $this->publishes([
-            realpath(__DIR__ . '/../../config/hotels.php') => config_path('hotels.php')
+            __DIR__ . '/../../config/hotels.php' 			=> config_path('hotels.php')
         ]);
 
         // register migrations
         $this->publishes([
-            __DIR__.'/../../database/migrations/' => base_path('/database/migrations'),
-            __DIR__.'/../../database/migrations/updates/' => base_path('/database/migrations/updates'),
+            __DIR__ . '/../../database/migrations/' 		=> base_path('/database/migrations'),
+            __DIR__ . '/../../database/migrations/updates/' => base_path('/database/migrations/updates'),
         ], 'migrations');
 
         // register seeds
         $this->publishes([
-            __DIR__.'/../../database/seeds/' => base_path('/database/seeds')
+            __DIR__ . '/../../database/seeds/' 				=> base_path('/database/seeds')
         ], 'seeds');
 	}
 
@@ -51,5 +52,4 @@ class HotelsServiceProvider extends ServiceProvider
 	{
         //
 	}
-
 }
