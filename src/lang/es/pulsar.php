d6e890e929dcd0ec28a6e5c279600dc212903620
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-04-16 12:07:21 +0200

    translations

--- src/lang/es/pulsar.php
+++ src/lang/es/pulsar.php
@@ -1,17 +1,6 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Admin Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used by the paginator library to build
-    | the pagination links. You're free to change them to anything you want.
-    | If you come up with something more exciting, let us know.
-    |
-    */
     'activities'                            => 'Actividades',
     'billing_data'                          => 'Datos de facturación',
     'booking_data'                          => 'Datos de reserva',
