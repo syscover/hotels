966ce80a4823f930daa142d35ed95d73db67fd60
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-04-27 18:01:14 +0200

    minor fix

--- src/database/seeds/HotelsPackageTableSeeder.php
+++ src/database/seeds/HotelsPackageTableSeeder.php
@@ -8,7 +8,7 @@ class HotelsPackageTableSeeder extends Seeder
     public function run()
     {
         Package::insert([
-            ['id_012' => '7', 'name_012' => 'Hotels Package', 'folder_012' => 'hotels', 'sorting_012' => 7, 'active_012' => '0']
+            ['id_012' => '7', 'name_012' => 'Hotels Package', 'folder_012' => 'hotels', 'sorting_012' => 7, 'active_012' => false]
         ]);
     }
 }
