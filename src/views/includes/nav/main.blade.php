a12ebc433a08aeacbbb0d05442761f09e2fa135a
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-09-20 19:12:59 +0200

    base_lang2()

--- src/views/includes/nav/main.blade.php
+++ src/views/includes/nav/main.blade.php
@@ -2,7 +2,7 @@
             <a href="javascript:void(0)"><i class="fa fa-h-square"></i>{{ trans('hotels::pulsar.package_name') }}</a>
             <ul class="sub-menu">
                 @if(is_allowed('hotels-hotel', 'access'))
-                    <li{!! is_current_resource('hotels-hotel') !!}><a href="{{ route('hotel', [base_lang()->id_001]) }}"><i class="fa fa-h-square"></i>{{ trans_choice('hotels::pulsar.hotel', 2) }}</a></li>
+                    <li{!! is_current_resource('hotels-hotel') !!}><a href="{{ route('hotel', [base_lang2()->id_001]) }}"><i class="fa fa-h-square"></i>{{ trans_choice('hotels::pulsar.hotel', 2) }}</a></li>
                 @endif
                 <li{!! is_current_resource(['hotels-environment','hotels-decoration','hotels-relationship','hotels-service','hotels-publication','hotels-attachment-family'], true) !!}>
                     <a href="javascript:void(0)"><i class="icomoon-icon-grid"></i>{{ trans('pulsar::pulsar.master_tables') }}</a>
@@ -11,16 +11,16 @@
                             <li{!! is_current_resource('hotels-publication') !!}><a href="{{ route('hotelsPublication') }}"><i class="fa fa-object-ungroup"></i>{{ trans_choice('hotels::pulsar.publication', 2) }}</a></li>
                         @endif
                         @if(is_allowed('hotels-service', 'access'))
-                            <li{!! is_current_resource('hotels-service') !!}><a href="{{ route('hotelsService', [base_lang()->id_001]) }}"><i class="icomoon-icon-wand-2"></i>{{ trans_choice('hotels::pulsar.service', 2) }}</a></li>
+                            <li{!! is_current_resource('hotels-service') !!}><a href="{{ route('hotelsService', [base_lang2()->id_001]) }}"><i class="icomoon-icon-wand-2"></i>{{ trans_choice('hotels::pulsar.service', 2) }}</a></li>
                         @endif
                         @if(is_allowed('hotels-decoration', 'access'))
-                            <li{!! is_current_resource('hotels-decoration') !!}><a href="{{ route('hotelsDecoration', [base_lang()->id_001]) }}"><i class="fa fa-lightbulb-o"></i>{{ trans_choice('hotels::pulsar.decoration', 2) }}</a></li>
+                            <li{!! is_current_resource('hotels-decoration') !!}><a href="{{ route('hotelsDecoration', [base_lang2()->id_001]) }}"><i class="fa fa-lightbulb-o"></i>{{ trans_choice('hotels::pulsar.decoration', 2) }}</a></li>
                         @endif
                         @if(is_allowed('hotels-environment', 'access'))
-                            <li{!! is_current_resource('hotels-environment') !!}><a href="{{ route('hotelsEnvironment', [base_lang()->id_001]) }}"><i class="fa fa-picture-o"></i>{{ trans_choice('hotels::pulsar.environment', 2) }}</a></li>
+                            <li{!! is_current_resource('hotels-environment') !!}><a href="{{ route('hotelsEnvironment', [base_lang2()->id_001]) }}"><i class="fa fa-picture-o"></i>{{ trans_choice('hotels::pulsar.environment', 2) }}</a></li>
                         @endif
                         @if(is_allowed('hotels-relationship', 'access'))
-                            <li{!! is_current_resource('hotels-relationship') !!}><a href="{{ route('hotelsRelationship', [base_lang()->id_001]) }}"><i class="fa fa-group"></i>{{ trans_choice('hotels::pulsar.relationship', 2) }}</a></li>
+                            <li{!! is_current_resource('hotels-relationship') !!}><a href="{{ route('hotelsRelationship', [base_lang2()->id_001]) }}"><i class="fa fa-group"></i>{{ trans_choice('hotels::pulsar.relationship', 2) }}</a></li>
                         @endif
                     </ul>
                 </li>
